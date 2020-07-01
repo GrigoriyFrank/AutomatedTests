0ee33013b3720dc4d4b61f497557b75ef88d5096
Grigoriy Frank <martinlewton2150@gmail.com>
2020-07-01 09:45:43 +0800

    add blank test methods

--- src/main/java/ru/gregfrank/TestOne.java
+++ src/main/java/ru/gregfrank/TestOne.java
@@ -1,4 +1,18 @@
 package ru.gregfrank;
 
+import org.testng.annotations.Test;
+
 public class TestOne {
+
+    @Test
+    public void firstTestMethodTestOne(){
+
+
+    }
+
+    @Test
+    public void secondTestMethodTestOne(){
+
+
+    }
 }
--- src/main/java/ru/gregfrank/TestTwo.java
+++ src/main/java/ru/gregfrank/TestTwo.java
@@ -1,4 +1,18 @@
 package ru.gregfrank;
 
+import org.testng.annotations.Test;
+
 public class TestTwo {
+
+    @Test
+    public void firstTestMethodTestTwo(){
+
+
+    }
+
+    @Test
+    public void secondTestMethodTestTwo(){
+
+
+    }
 }
