cca3234c618c2dabd0cc3fbee88ce8e35ea2c1f0
Grigoriy Frank <martinlewton2150@gmail.com>
2020-07-06 15:56:21 +0800

    move generic test to BaseTest

---
+++ src/test/java/ru/gregfrank/testSuite/BaseTest.java
@@ -0,0 +1,39 @@
+package ru.gregfrank.testSuite;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class BaseTest {
+
+    public WebDriver webDriver;
+
+    @AfterClass
+    public void tearDown() {
+
+        if (webDriver != null) {
+            webDriver.quit();
+        }
+
+    }
+
+    @Parameters("browser")
+    @Test
+    public void openBrowser(String browser){
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            webDriver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            webDriver = new FirefoxDriver();
+        } else {
+            throw new IllegalArgumentException("Check 'browser' argument! It must be either 'chrome' or 'firefox'");
+        }
+
+        webDriver.manage().window().maximize();
+        webDriver.get("https://www.google.com/");
+
+    }
+}
--- src/test/java/ru/gregfrank/testSuite/TestOne.java
+++ src/test/java/ru/gregfrank/testSuite/TestOne.java
@@ -9,32 +9,6 @@ import org.testng.annotations.Test;
 
 public class TestOne {
 
-    public WebDriver webDriver;
 
-    @AfterClass
-    public void tearDown() {
-
-        if (webDriver != null) {
-            webDriver.quit();
-        }
-
-    }
-
-    @Parameters("browser")
-    @Test
-    public void openBrowser(String browser){
-
-        if (browser.equalsIgnoreCase("chrome")) {
-            webDriver = new ChromeDriver();
-        } else if (browser.equalsIgnoreCase("firefox")) {
-            webDriver = new FirefoxDriver();
-        } else {
-            throw new IllegalArgumentException("Check 'browser' argument! It must be either 'chrome' or 'firefox'");
-        }
-
-        webDriver.manage().window().maximize();
-        webDriver.get("https://www.google.com/");
-
-    }
 
 }
