e62b20911521da4c2836819300e7740bfa6444c0
Grigoriy Frank <martinlewton2150@gmail.com>
2020-07-06 15:58:24 +0800

    add extends

--- src/test/java/ru/gregfrank/testSuite/TestOne.java
+++ src/test/java/ru/gregfrank/testSuite/TestOne.java
@@ -7,7 +7,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class TestOne {
+public class TestOne extends BaseTest {
 
 
 
