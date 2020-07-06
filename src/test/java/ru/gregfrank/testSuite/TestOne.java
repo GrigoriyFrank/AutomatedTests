75e673149209b46b0ba47715d5b87aabfa90e667
Grigoriy Frank <martinlewton2150@gmail.com>
2020-07-06 15:28:52 +0800

    add parallel tests

--- src/test/java/ru/gregfrank/testSuite/TestOne.java
+++ src/test/java/ru/gregfrank/testSuite/TestOne.java
@@ -1,18 +1,40 @@
 package ru.gregfrank.testSuite;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TestOne {
 
+    public WebDriver webDriver;
+
+    @AfterClass
+    public void tearDown() {
+
+        if (webDriver != null) {
+            webDriver.quit();
+        }
+
+    }
+
+    @Parameters("browser")
     @Test
-    public void firstTestMethodTestOne(){
+    public void openBrowser(String browser){
 
+        if (browser.equalsIgnoreCase("chrome")) {
+            webDriver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            webDriver = new FirefoxDriver();
+        } else {
+            throw new IllegalArgumentException("Check 'browser' argument! It must be either 'chrome' or 'firefox'");
+        }
+
+        webDriver.manage().window().maximize();
+        webDriver.get("https://www.google.com/");
 
     }
 
-    @Test(dependsOnMethods = "firstTestMethodTestOne")
-    public void secondTestMethodTestOne(){
-
-
-    }
 }
