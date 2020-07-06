598fc25a1b93c501931ed6b49e08ef6fc68de538
Grigoriy Frank <martinlewton2150@gmail.com>
2020-07-06 18:33:39 +0800

    add blank tests

--- src/test/java/ru/gregfrank/testSuite/TestOne.java
+++ src/test/java/ru/gregfrank/testSuite/TestOne.java
@@ -3,12 +3,30 @@ package ru.gregfrank.testSuite;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TestOne extends BaseTest {
 
+    @Test
+    public void testMethod1(){
 
+    }
 
+    @Test
+    public void testMethod2(){
+
+    }
+
+    @Test
+    public void testMethod3(){
+
+    }
+
+    @Test
+    public void testMethod4(){
+
+    }
 }
