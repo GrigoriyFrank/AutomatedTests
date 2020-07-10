e70fdb61aa2dc8957a93359ed8c09ae5271e1b7a
Grigoriy Frank <martinlewton2150@gmail.com>
2020-07-10 11:59:02 +0800

    approach to use Fluent API

--- src/test/java/ru/gregfrank/testAutomation/PageObjects/BasePage.java
+++ src/test/java/ru/gregfrank/testAutomation/PageObjects/BasePage.java
@@ -2,13 +2,24 @@ package ru.gregfrank.testAutomation.PageObjects;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.LoadableComponent;
 
-public class BasePage {
+public class BasePage extends LoadableComponent<BasePage> {
 
     protected static WebDriver driver;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
+    }
+
+    @Override
+    protected void load() {
+
+    }
+
+    @Override
+    protected void isLoaded() throws Error {
+
     }
 }
